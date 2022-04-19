eca8a644c79ed0bc5b442f6d1a4ced0a749c5696
Parthpatodi <patodiparth@gmail.com>
2022-04-19 16:50:17 +0530

    men

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { WomenSubcategoryComponent } from './women-subcategory/women-subcategory.component';
@@ -15,7 +14,6 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { MainHomeComponent } from './main-home/main-home.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutUsComponent } from './about-us/about-us.component';
-
 
 @NgModule({
   declarations: [
