a2356ab1f99324b5d8293e6cda6a1a2ec83002c3
Parthpatodi <patodiparth@gmail.com>
2022-04-20 17:39:54 +0530

    user

--- src/app/search-product.service.ts
+++ src/app/search-product.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class SearchProductService {
-  searchProductApi = "https://vivah-backend.herokuapp.com/user/search-food";
+  searchProductApi = "https://vivah-backend-api.herokuapp.com/user/search-food";
 
   getSearchResult(text:string){
     return this.http.post<any>(this.searchProductApi,{productName:text});
